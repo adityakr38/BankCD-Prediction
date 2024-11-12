2ab100dcd37ec3306bdd9e37b122043151084eab
adityakr38 <kumaraditya233007@gmail.com>
2024-11-12 15:20:31 -0500

    DataIngestion-Added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.bankcdProject import logger
+from src.bankcdProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is my custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
